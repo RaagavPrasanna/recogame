5537a850fc31a489c054852d00fbdd502f3a9883
nenikitov <nenikitov@mail.ru>
2023-02-08 11:11:19 -0500

    Set up simple express server

---
+++ server/src/app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const PORT = 3000;
+const app = express();
+
+async function main() {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+
+  app.get('/*', (_, res) => {
+    res.send('Working');
+  });
+}
+
+main();
+
