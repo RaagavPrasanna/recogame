7089df43f9029e7abdfafeddc294a021bd2ed681
Raagav Prasanna <raagav.pras@gmail.com>
2023-02-09 12:33:37 -0500

    Create proj and add gitignore

---
+++ db_app/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace db_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
