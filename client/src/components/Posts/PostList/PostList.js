d6487772bba0986889b422d5c084a4e1af653bc2
Paul Patrick Rabanal <paul7098@hotmail.com>
2023-02-15 09:22:04 -0500

    Show game posts

--- client/src/components/Posts/PostList/PostList.js
+++ client/src/components/Posts/PostList/PostList.js
@@ -1,5 +1,19 @@
+import GamePost from '../GamePost/GamePost';
+
 function PostList({ posts }) {
-  return <></>;
+  const availablePosts = posts.map((post, i) => {
+    return (
+      <GamePost
+        key={i}
+        gameTitle={post.gameTitle}
+        devName={post.devName}
+        review={post.review}
+        rating={post.rating}
+      />
+    );
+  });
+
+  return <ul>{availablePosts}</ul>;
 }
 
 export default PostList;
