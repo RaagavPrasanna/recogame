0861ed53dda7db3c704c76d86b8aa6f69ab59eee
Paul Patrick Rabanal <paul7098@hotmail.com>
2023-02-15 09:25:35 -0500

    Show star ratings

--- client/src/components/Posts/GamePost/GamePost.js
+++ client/src/components/Posts/GamePost/GamePost.js
@@ -1,11 +1,14 @@
 function GamePost({ gameTitle, devName, review, rating }) {
   return (
     <li>
-      <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" width={150}/>
+      <img
+        src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+        width={150}
+      />
       <h1>{gameTitle}</h1>
       <div>
         <span>{devName}</span>
-        <span>{rating}</span>
+        <span>{'⭐️'.repeat(rating)}</span>
       </div>
       <p>{review}</p>
     </li>
