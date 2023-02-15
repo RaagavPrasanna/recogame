07c69cf14749623d56374278a76e8a130be75e19
Paul Patrick Rabanal <paul7098@hotmail.com>
2023-02-15 10:11:25 -0500

    Add infinite scrolling

--- client/src/components/Posts/PostList/PostList.js
+++ client/src/components/Posts/PostList/PostList.js
@@ -1,9 +1,29 @@
 import { useState } from 'react';
 import GamePost from '../GamePost/GamePost';
 import { mockGamePosts } from '../../../MockData/MockGamePosts';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 function PostList() {
   const [posts, setPosts] = useState(mockGamePosts);
+
+  function fetchMoreData() {
+    setTimeout(() => {
+      setPosts((prevPosts) => {
+        return prevPosts.concat(
+          Array.from({ length: 20 }).map(() => {
+            return {
+              gameTitle: 'Game Name',
+              devName: 'Dev Name',
+              review:
+                // eslint-disable-next-line max-len
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+              rating: 5,
+            };
+          })
+        );
+      });
+    }, 1500);
+  }
 
   const availablePosts = posts.map((post, i) => {
     return (
@@ -17,7 +37,19 @@ function PostList() {
     );
   });
 
-  return <ul>{availablePosts}</ul>;
+  return (
+    <ul>
+      <InfiniteScroll
+        dataLength={10}
+        next={fetchMoreData}
+        hasMore={true}
+        loader={<h4>Loading...</h4>}
+        height={700}
+      >
+        {availablePosts}
+      </InfiniteScroll>
+    </ul>
+  );
 }
 
 export default PostList;
