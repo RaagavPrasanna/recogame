11e627e91460426229fe81745fe2b0e94cd03d01
Paul Patrick Rabanal <paul7098@hotmail.com>
2023-02-15 09:21:39 -0500

    Add props

--- client/src/components/Posts/GamePost/GamePost.js
+++ client/src/components/Posts/GamePost/GamePost.js
@@ -1,5 +1,15 @@
-function GamePost({ gameTitle, devName, review, rating, imgSrc }) {
-  return <></>;
+function GamePost({ gameTitle, devName, review, rating }) {
+  return (
+    <li>
+      <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" width={150}/>
+      <h1>{gameTitle}</h1>
+      <div>
+        <span>{devName}</span>
+        <span>{rating}</span>
+      </div>
+      <p>{review}</p>
+    </li>
+  );
 }
 
 export default GamePost;
