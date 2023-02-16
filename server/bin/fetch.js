66478af1c7f76a64a0177e0b4f0b5cc0054afc9f
nenikitov <nenikitov@mail.ru>
2023-02-16 12:08:48 -0500

    Wrote arguments

--- server/bin/fetch.js
+++ server/bin/fetch.js
@@ -4,15 +4,30 @@ import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 import '../src/env/env.js';
 
-// Arguments
+
 const argv = yargs(hideBin(process.argv))
   .command(
-    'steam', 'Fetch infomration from Steam API', {
-      type: {
-        describe: 'Kind of infomration to fetch from steam',
-        alias: 't',
-        demand: true
-      }
+    // Steam
+    'steam', 'Fetch some data from steam',
+    (yargs) => {
+      return yargs
+        // All
+        .command(
+          'all', 'Fetch all IDs and names'
+        )
+        // Info <id>
+        .command(
+          'info', 'Fetch info about a game with specified ID',
+          (yargs) => {
+            yargs.option(
+              'i', {
+                alias: 'id',
+                type: 'number',
+                demandOption: true
+              }
+            );
+          }
+        );
     }
   )
   .help()
