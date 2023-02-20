4d0187c419c017dfd08fca698571fc735a5cd6ee
Paul Patrick Rabanal <paul7098@hotmail.com>
2023-02-20 17:58:36 -0500

    Add header bg color fade in

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,10 +1,29 @@
 import Button from '../UI/Button/Button';
 import styles from './Header.module.css';
 import { Link } from 'react-router-dom';
+import { useEffect, useRef, useState } from 'react';
 
 function Header() {
+  const [navBg, setNavBg] = useState(false);
+  const headerRef = useRef();
+
+  const changeNavBg = () => {
+    window.scrollY >= headerRef.current.offsetHeight ? setNavBg(true) : setNavBg(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', changeNavBg);
+    return () => {
+      window.removeEventListener('scroll', changeNavBg);
+    };
+  }, []);
+
   return (
-    <div className={styles.header}>
+    <div
+      className={`${styles.header} ${navBg && styles.showBg}`}
+      onScroll={changeNavBg}
+      ref={headerRef}
+    >
       <header className={styles.buttons}>
         <Link to="/">
           <Button> Home </Button>
