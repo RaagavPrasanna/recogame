43bd62081085fd2175de8b38f44c59c472df713e
Liliane Tran Le <lylyanetl@hotmail.com>
2023-02-20 17:56:41 -0500

    Initialize the game list

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,14 +1,16 @@
 import Header from './components/Header/Header';
-import PostList from './components/Posts/PostList/PostList';
+// import PostList from './components/Posts/PostList/PostList';
 // import Register from './components/Forms/RegisterForm';
 // import Login from './components/Forms/LoginForm';
+import GameList from './components/GameList/GameList';
 function Home() {
   return (
     <>
       <Header />
-      <PostList />
+      {/* <PostList /> */}
       {/* <Login /> */}
       {/* <Register /> */}
+      <GameList />
     </>
   );
 }
---
+++ client/src/components/GameList/GameList.js
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import Button from '../UI/Button/Button';
+import classes from './GameList.module.css';
+
+function GameList() {
+  const [checked, setChecked] = useState(false);
+
+  const handleChange = () => {
+    setChecked(!checked);
+  };
+
+  return (
+    <div className={classes.gameList}>
+      <h3> Wishlist </h3>
+      <article>
+        <input type="checkbox" onChange={handleChange} />
+        Lorem ipsum dolor sit amet.
+      </article>
+      <br />
+      <Button> Remove </Button>
+      <h3> In progress</h3>
+
+      <h3> Finished </h3>
+
+      <h3> Completed 100% Achievements </h3>
+
+      <h3> Never Played </h3>
+    </div>
+  );
+}
+
+export default GameList;
