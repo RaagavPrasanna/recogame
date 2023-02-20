18c9aa8e947a33c876cf5d77d12df4bdeb41e01c
Shirley Maa <maashirley@gmail.com>
2023-02-20 11:42:24 -0500

    fix eslint

--- server/src/db/models.js
+++ server/src/db/models.js
@@ -2,11 +2,11 @@ import mongoose from 'mongoose';
 
 /**list of games with appid and name */
 const AllGames = mongoose.model(
-  'all-games', 
+  'all-games',
   new mongoose.Schema({
     appid: {
       type: Number,
-      required: true, 
+      required: true,
       unique: true
     },
     name: String,
@@ -14,12 +14,12 @@ const AllGames = mongoose.model(
 );
 
 const GameDetails = mongoose.model(
-  'game-details', 
+  'game-details',
   new mongoose.Schema({
     steamId: {
       type: Number,
-      required: [true, 'steamId is requird'], 
-      unique: true          
+      required: [true, 'steamId is requird'],
+      unique: true
     },
     name: String,
     developers: [String],
@@ -32,13 +32,13 @@ const GameDetails = mongoose.model(
     detailedDescription: String,
     shortDescription: String,
     supportedLanguages: [String],
-    platforms: [String],    
+    platforms: [String],
     metacritic: String,
     screenshots: [String],
     movies: [String],
     recommendations: Number,
     background: String,
-    contentDescriptors: String 
+    contentDescriptors: String
   })
 );
 
