9f3b4fd4e028e8822ff17e18927e3729b677f5b2
Liliane Tran Le <lylyanetl@hotmail.com>
2023-02-20 12:34:19 -0500

    Commented the registration and login

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,14 +1,14 @@
 import Header from './components/Header/Header';
-// import PostList from './components/Posts/PostList/PostList';
-import Register from './components/Forms/RegisterForm';
+import PostList from './components/Posts/PostList/PostList';
+// import Register from './components/Forms/RegisterForm';
 // import Login from './components/Forms/LoginForm';
 function Home() {
   return (
     <>
       <Header />
-      {/* <PostList /> */}
+      <PostList />
       {/* <Login /> */}
-      <Register />
+      {/* <Register /> */}
     </>
   );
 }
