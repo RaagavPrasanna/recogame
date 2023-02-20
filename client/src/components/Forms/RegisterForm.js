c5c65c81619bbc039d1311eb603e8e8cc7b988ff
Liliane Tran Le <lylyanetl@hotmail.com>
2023-02-20 12:33:56 -0500

    Done registration form

---
+++ client/src/components/Forms/RegisterForm.js
@@ -0,0 +1,83 @@
+import { useRef, useState } from 'react';
+import Button from '../UI/Button/Button';
+import Modal from '../UI/Modal/Modal';
+import classes from './RegisterForm.module.css';
+
+function RegisterForm({ onCancel, isValid }) {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  const [email, setEmail] = useState();
+  const [gameTags, setGameTags] = useState();
+  const [profilePic, setProfilePicture] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <Modal onClick={onCancel}>
+      <form className={classes.registerForm}>
+        <Button> Log in with Steam</Button>
+        <h2> Register </h2>
+        <label> Username </label>
+        <br />
+        <input
+          id="username"
+          type="text"
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+        <br />
+        <br />
+        <label> Password </label>
+        <br />
+        <input
+          id="password"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <br />
+        <br />
+        <label> Email </label>
+        <br />
+        <input
+          id="email"
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <br />
+        <br />
+        <label> Game Tags </label>
+        <br />
+        <input
+          id="gameTags"
+          type="text"
+          onChange={(e) => setGameTags(e.target.value)}
+          required
+        />
+        <br />
+        <br />
+        <label> Profile Picture </label>
+        <br />
+        <input
+          id="profilePicture"
+          type="file"
+          accept="image/*"
+          multiple="false"
+          // ref={useRef}
+        />
+        <br />
+        <br />
+        <div>
+          <Button onClick={() => handleSubmit()}> Register </Button>
+          <Button onClick={() => onCancel()}>Cancel</Button>
+        </div>
+      </form>
+      {/* <div>{isValid || 'Username must contain at least 4 characters.'}</div> */}
+    </Modal>
+  );
+}
+
+export default RegisterForm;
