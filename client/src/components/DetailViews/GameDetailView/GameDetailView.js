fd99d9d1626662024584810320f140fa305d99e9
Paul Patrick Rabanal <paul7098@hotmail.com>
2023-02-20 12:09:34 -0500

    Add HTML code

--- client/src/components/DetailViews/GameDetailView/GameDetailView.js
+++ client/src/components/DetailViews/GameDetailView/GameDetailView.js
@@ -1,5 +1,32 @@
-function GameDetailView({ id }) {
-  
+import { useReducer } from 'react';
+import Button from '../../UI/Button/Button';
 
-  return <></>;
+function gameReducer(state, action) {}
+
+function GameDetailView({ id }) {
+  const [gameDetails, dispatchGameDetails] = useReducer(gameReducer, {
+    gameName: '',
+    gamePrice: 0,
+    gameImgSrc: '',
+    gameDesc: '',
+    reviews: [],
+  });
+
+  return (
+    <>
+      <h1>{gameDetails.gameName}</h1>
+      <img src={gameDetails.gameImgSrc} />
+      <div>
+        <span>{gameDetails.gamePrice}</span>
+        <Button>BUY</Button>
+        <Button>ADD TO WISHLIST</Button>
+        <Button>ADD TO MY GAMELIST</Button>
+        {/* TODO: Drop down menu */}
+      </div>
+      <div>
+        <h2>Community Review</h2>
+        {/* TODO: Render reviews */}
+      </div>
+    </>
+  );
 }
