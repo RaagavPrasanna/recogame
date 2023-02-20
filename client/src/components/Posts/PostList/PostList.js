772fc0486bca32062da1fb7b635c8150538fd315
Paul Patrick Rabanal <paul7098@hotmail.com>
2023-02-20 13:34:19 -0500

    Fix styling

--- client/src/components/Posts/PostList/PostList.js
+++ client/src/components/Posts/PostList/PostList.js
@@ -16,7 +16,7 @@ function PostList() {
             return {
               gameTitle: 'Game Name',
               devName: 'Dev Name',
-              review:
+              description:
                 // eslint-disable-next-line max-len
                 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
               rating: 5,
@@ -33,7 +33,7 @@ function PostList() {
         key={i}
         gameTitle={post.gameTitle}
         devName={post.devName}
-        review={post.review}
+        description={post.description}
         rating={post.rating}
       />
     );
