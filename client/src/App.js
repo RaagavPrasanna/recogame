cd9856fc9c6b3fd62366f4e8bb004fcd5478041b
Paul Patrick Rabanal <paul7098@hotmail.com>
2023-02-21 18:38:41 -0500

    Make header global

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
 import Home from './Home';
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
