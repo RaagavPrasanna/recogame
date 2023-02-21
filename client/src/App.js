b74a87d5289228a1bf9d7196c1d82c9ff9667156
Paul Patrick Rabanal <paul7098@hotmail.com>
2023-02-21 18:56:40 -0500

    Add route for GameDetailView

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './Home';
+import GameDetailView from './components/DetailViews/GameDetailView/GameDetailView';
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          {/* TODO: GameDetailView id prop should change dynamically depending on the id in the path */}
+          <Route path="/game/:id" element={<GameDetailView id={1} />} />
         </Routes>
       </BrowserRouter>
     </>
