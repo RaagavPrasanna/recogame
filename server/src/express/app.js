925c4c3564f587868028fd7dfac42b1e7a71c33c
Raagav Prasanna <raagav.pras@gmail.com>
2023-02-22 11:08:56 -0500

    Added swagger annotation for all-games endpoint

--- server/src/express/app.js
+++ server/src/express/app.js
@@ -10,22 +10,41 @@ import swaggerUi from 'swagger-ui-express';
 
 const app = express();
 
-const swaggerOptions = {
-  swaggerDefinition: {
-    info: {
-      title: 'Rest API',
-      description: 'A simple rest API',
-      servers: ['http://localhost:3001']
-    }
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Express API for JSONPlaceholder',
+    version: '1.0.0',
+    description:
+      'This is a REST API application made with Express. It retrieves data from JSONPlaceholder.',
+    license: {
+      name: 'Licensed Under MIT',
+      url: 'https://spdx.org/licenses/MIT.html',
+    },
+    contact: {
+      name: 'JSONPlaceholder',
+      url: 'https://jsonplaceholder.typicode.com',
+    },
   },
-  apis: ['./routes/*.js']
+  servers: [
+    {
+      url: 'http://localhost:3000',
+      description: 'Development server',
+    },
+  ],
 };
 
-const specs = swaggerJsdoc(swaggerOptions);
+
+const options = {
+  apis: ['./routes/api.js'],
+  swaggerDefinition,
+};
+
+const swaggerSpec = swaggerJsdoc(options);
 app.use(
   '/api-docs',
   swaggerUi.serve,
-  swaggerUi.setup(specs)
+  swaggerUi.setup(swaggerSpec)
 );
 
 // API
