e934276235a3dfbcdbd8ae711601db98bbd1c63f
Shirley Maa <maashirley@gmail.com>
2023-02-25 16:37:25 -0500

    getOneGameDetails() in NodeReader.java done

--- db_java/src/main/java/ca/recogame/App.java
+++ db_java/src/main/java/ca/recogame/App.java
@@ -20,18 +20,67 @@ import java.util.List;
 public class App 
 {
     //this code is terrible! Always code in OO style with Java
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException, InterruptedException
     {
-        // Game newGame = new Game(8891, "testJava4");
+        // Game newGame = new Game(8891, "testJava4")
         // Connection connect = new Connection();
         // connect.insertOneGame(newGame);
         // -------------------test for get all Games from node console-----------------------------------
+        // try{
+        //     NodeReader nReader = new NodeReader();
+        //     List<Game> games = nReader.getAllGames("IGDB");
+        //     System.out.println(games.get(5).getAppid());
+        // }catch(Exception e){
+        //     System.out.println("can not data shown : "+e);
+        // }
+        // ---------------test for get 1 game details----
         try{
             NodeReader nReader = new NodeReader();
-            List<Game> games = nReader.getAllGames();
+            GameDetails game = nReader.getOneGameDetails("IGDB", 440);
+            System.out.println(game.getName());
         }catch(Exception e){
-
+            System.out.println("can not data shown : "+e);
         }
+        // --------------------test for 1 game-----------------
+        // String name = "steam";
+        // String id = "10";
+        // Gson gson = new Gson();
+        // Type gameListType = new TypeToken<List<GameDetails>>() {}.getType();
+        //     List<GameDetails> allgameDetails = new ArrayList<GameDetails>();
+        //     GameDetails gameDetails = new GameDetails();
+        //     // String command = "./server/bin/fetch.js "+ name + " info --id " + id ;
+        //     // System.out.println(command);
+        //     // set/start ProcessBuilder
+        //     ProcessBuilder processBuilder = new ProcessBuilder("node", "./server/bin/fetch.js", name, "info","--id", id);
+        //     // ProcessBuilder processBuilder = new ProcessBuilder("node", command);
+        //     Process process = processBuilder.start();
+        //     // get the output stream of the process
+        //     InputStream stdout = process.getInputStream();
+        //     try (BufferedReader reader = new BufferedReader(
+        //             new InputStreamReader(stdout))) {
+        //         Thread task = new Thread(() -> {
+        //             String line;
+        //             try {
+        //                 while ((line = reader.readLine()) != null) {                
+        //                     // System.out.println(line);
+        //                     GameDetails newGame = gson.fromJson(line, GameDetails.class);
+        //                     allgameDetails.add(newGame);                   
+        //                     System.out.println(newGame.getName());
+                            
+        //                 }                    
+        //             } catch (IOException e) {
+        //                 e.printStackTrace();
+        //             }
+        //         });
+        //         task.start();
+        //         task.join();
+        //         // wait for the process to finish
+        //         int exitCode = process.waitFor();
+        //         System.out.println("Process exited with code " + exitCode);
+        //         // System.out.println(allgames.get(100).getAppid());
+        //         System.out.println( allgameDetails.get(0));
+        //     }
+
     }
 }
       
--- db_java/src/main/java/ca/recogame/NodeReader.java
+++ db_java/src/main/java/ca/recogame/NodeReader.java
@@ -12,52 +12,127 @@ import java.lang.reflect.Type;
 
 public class NodeReader {
 
-    private Gson gson ;
+    private Gson gson;
 
-    public NodeReader(){
+    public NodeReader() {
         this.gson = new Gson();
     }
     /**
-     * ProcessBuilder run node command, take the standout as json data, convert to List<Game>
+     * ProcessBuilder run node command, take the standout as json data, convert to
+     * List<Game>
+     * 
      * @return List<Game> of game in Game Object
      * @throws IOException
      * @throws InterruptedException
      */
-    public List<Game> getAllGames() throws IOException, InterruptedException {
-        // setup Gson for List<Game> 
-        Type gameListType = new TypeToken<List<Game>>(){}.getType();
-        List<Game> allgames = new ArrayList<Game>();
-        // set/start ProcessBuilder
-        ProcessBuilder processBuilder = new ProcessBuilder("node", "./server/bin/fetch.js", "steam", "all");
-        Process process = processBuilder.start();
-        // get the output stream of the process
-        InputStream stdout = process.getInputStream();
-        try(BufferedReader reader = new BufferedReader(
-            new InputStreamReader(stdout))){
-            Thread task = new Thread(()->{
-                String line;
-                try {
-                    while ((line = reader.readLine()) != null) {
-                    // convert json data to List<Game>
-                    List<Game> games = this.gson.fromJson(line, gameListType);
-                        for (Game g : games){
-                            allgames.add(g);
-                        
+    public List<Game> getAllGames(String website) throws 
+                IOException, 
+                InterruptedException 
+    {
+        // valide input string
+        String name = validateName(website);
+        if (name != null) {
+            // setup Gson for List<Game>
+            Type gameListType = new TypeToken<List<Game>>() {}.getType();
+            List<Game> allgames = new ArrayList<Game>();
+            // set/start ProcessBuilder
+            ProcessBuilder processBuilder = 
+                new ProcessBuilder("node", "./server/bin/fetch.js", name, "all");
+            Process process = processBuilder.start();
+            // get the output stream of the process
+            InputStream stdout = process.getInputStream();
+            try (BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(stdout))) {
+                Thread task = new Thread(() -> {
+                    String line;
+                    try {
+                        while ((line = reader.readLine()) != null) {
+                            // convert json data to List<Game>
+                            List<Game> games = 
+                                this.gson.fromJson(line, gameListType);
+                            for (Game g : games) {
+                                allgames.add(g);
+                            }
                         }
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });
-            task.start();
-            task.join();
-        // wait for the process to finish
-        int exitCode = process.waitFor();
-        System.out.println("Process exited with code " + exitCode);
-        System.out.println(allgames.get(100).getAppid());
-        return allgames;
-      }
+                });
+                task.start();
+                task.join();
+                // wait for the process to finish
+                int exitCode = process.waitFor();
+                System.out.println("Process exited with code " + exitCode);
+                // System.out.println(allgames.get(100).getAppid());
+                return allgames;
+            }
+        } else {
+            return null;
+        }
+    }
+/**
+ * ProcessBuilder run node command, take the standout as json data, convert to 
+ * GameDetails object
+ * @param website - name
+ * @param id of the game
+ * @return GameDetails
+ * @throws IOException
+ * @throws InterruptedException
+ */
+    public GameDetails getOneGameDetails(String website, int id) throws 
+                IOException, 
+                InterruptedException 
+    {
+        // valide input string
+        String name = validateName(website);
+        if (name != null || id > 0) {
+            String idStr = Integer.toString(id);
+            List<GameDetails> gameDetails = new ArrayList<GameDetails>();
+            
+            // set/start ProcessBuilder- all args have to be String
+            ProcessBuilder processBuilder = 
+                new ProcessBuilder("node", "./server/bin/fetch.js", name,"info", "--id", idStr);
+            
+            // ProcessBuilder processBuilder = new ProcessBuilder("node", command);
+            Process process = processBuilder.start();
+            
+            // get the output stream of the process
+            InputStream stdout = process.getInputStream();
+          
+            try (BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(stdout))) {
+                Thread task = new Thread(() -> {
+                    try {
+                        String line;
+                        while ((line = reader.readLine()) != null) {                
+                            GameDetails newGame = 
+                                gson.fromJson(line, GameDetails.class);
+                            // send the object to outside
+                            gameDetails.add(newGame);
+                        }                    
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+                task.start();
+                task.join();
+                // wait for the process to finish
+                int exitCode = process.waitFor();
+                System.out.println("Process exited with code " + exitCode);
+                return gameDetails.get(0);
+            }
+        } else {
+            return null;
+        }
     }
 
-    
+
+    private String validateName(String website) {
+        String name = website.toLowerCase();
+        if (!(name.equals("steam") || name.equals("igdb"))) {
+            System.out.println("Can't read from Node, no such website name : " + website);
+            return null;
+        }
+        return name;
+    }
 }
