e3cfe1f6d5c808a5575b943d79db3e783474d1fe
Raagav Prasanna <raagav.pras@gmail.com>
2023-02-27 18:07:52 -0500

    send user back to client

--- server/src/express/routes/authentication.js
+++ server/src/express/routes/authentication.js
@@ -1,8 +1,31 @@
 import express from 'express';
 import { OAuth2Client } from 'google-auth-library';
+import session from 'express-session';
 
 const router = express.Router();
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+
+const users = new Array();
+
+function isAuthenticated(req, res, next) {
+  if(!req.session.user) {
+    return res.sendStatus(401);
+  }
+  next();
+}
+
+router.use(session({
+  secret: process.env.SECRET,
+  name: 'id',
+  saveUninitialized: false,
+  resave: false,
+  cookie : {
+    maxAge: 120000,
+    secure: false,
+    httpOnly: true,
+    sameSite: 'strict'
+  }
+}));
 
 router.post('/auth', async (req, res) => {
   const { token } = req.body;
@@ -15,7 +38,36 @@ router.post('/auth', async (req, res) => {
   }
   const { name, email, picture } = ticket.getPayload();
 
-  console.log(name, email, picture);
+  const user = { 'name': name, 'email': email, 'picture': picture };
+  const existsAlready = users.find(u => u.email === user.email);
+
+  if(existsAlready) {
+    users.push(user);
+  } else {
+    users[existsAlready] = user;
+  }
+
+  req.session.regenerate(function(err) {
+    if(err) {
+      return res.sendStatus(500);
+    }
+    req.session.user = user;
+    res.json({ user: user });
+  });
+});
+
+router.get('/protected', isAuthenticated, function(req, res) {
+  res.sendStatus(200);
+});
+
+router.get('/logout', isAuthenticated, function(req, res) {
+  req.session.destroy(function(err) {
+    if(err) {
+      return res.sendStatus(500);
+    }
+    res.clearCookie('id');
+    res.sendStatus(200);
+  });
 });
 
 export default router;
