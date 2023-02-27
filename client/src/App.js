d703bd7483f08107694bffa6550ccf68f5144aeb
Paul Patrick Rabanal <paul7098@hotmail.com>
2023-02-27 12:34:39 -0500

    Add routes for user profile and settings

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './Home';
 import GameDetailView from './components/DetailViews/GameDetailView/GameDetailView';
+import UserProfile from './components/User/UserProfile/UserProfile';
+import UserSettings from './components/User/UserSettings/UserSettings';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/game/:id" element={<GameDetailView />} />
+          <Route path="/profile" element={<UserProfile />} />
+          <Route path="/profile/settings" element={<UserSettings />} />
         </Routes>
       </BrowserRouter>
     </>
