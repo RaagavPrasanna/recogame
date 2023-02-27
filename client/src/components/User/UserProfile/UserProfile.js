8fdc7a42cdfdb97bdac8195ab4d8ebf5a8731927
Paul Patrick Rabanal <paul7098@hotmail.com>
2023-02-27 12:24:00 -0500

    Add user profile picture and settings button

--- client/src/components/User/UserProfile/UserProfile.js
+++ client/src/components/User/UserProfile/UserProfile.js
@@ -1,7 +1,15 @@
 import styles from './UserProfile.module.css';
 
-function UserProfile() {
-  return <div className={styles['user-profile']}></div>;
+function UserProfile({ username }) {
+  return (
+    <div className={styles['user-profile']}>
+      <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" />
+      <div className={styles['user-info']}>
+        <h2>{username}</h2>
+        <span className={styles.settings}>User Settings</span>
+      </div>
+    </div>
+  );
 }
 
 export default UserProfile;
