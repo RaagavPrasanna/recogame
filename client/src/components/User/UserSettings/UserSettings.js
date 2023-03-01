4a05a0d29a243ffde9b6ce1bba546c71b1fea258
Paul Patrick Rabanal <paul7098@hotmail.com>
2023-03-01 11:11:46 -0500

    Add todo for useEffect

--- client/src/components/User/UserSettings/UserSettings.js
+++ client/src/components/User/UserSettings/UserSettings.js
@@ -2,12 +2,14 @@ import styles from './UserSettings.module.css';
 import Modal from '../../UI/Modal/Modal.js';
 import SettingsSwitch from './SettingsSwitch';
 import Button from '../../UI/Button/Button';
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 function UserSettings({ onCancel }) {
   const shareDataRef = useRef();
 
-  //TODO: Add use effect to get user settings from the server
+  //TODO: Add use effect to get user settings from the server.
+  //      Add asn isChecked prop to the SettingsSwitch component.
+  useEffect(() => {}, []);
 
   function saveSettings() {
     console.log('shareData: ', shareDataRef.current.isEnabled);
