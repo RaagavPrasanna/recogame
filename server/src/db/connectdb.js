0a37ad76f36393eae1e689257f544feb317a3f58
Shirley Maa <maashirley@gmail.com>
2023-02-28 19:51:09 -0500

    test db connection with change of new fields

--- server/src/db/connectdb.js
+++ server/src/db/connectdb.js
@@ -1,7 +1,8 @@
+
 //remove ../env/env.js ---------before merge
 import '../env/env.js';
 import db from './db.js';
-import api from '../../controller/steamapi/steam_api.js';
+import api from '../controller/steamapi/steam_api.js';
 import queries from './queries.js';
 
 /**
@@ -11,11 +12,11 @@ import queries from './queries.js';
 async function steamGameToMongo(){
   try{
     let games = await api.fetchAllSteamApps();
-    await db.connect('620-reco-test1');
+    await db.connect('620-reco-test2');
     await queries.insertAllGames(games);
   } catch (e){
     console.log('can not insert game info in all-games');
-  }
+  } 
 }
 
 /**
@@ -25,7 +26,8 @@ async function steamGameToMongo(){
  */
 async function oneGameToMongo(id){
   let game = await api.fetchGameInfo(id);
-  await db.connect('620-reco-test1');
+  console.log(game);
+  await db.connect('620-reco-test2');
   await queries.insertOneGame(game);
 }
 /**
@@ -34,11 +36,16 @@ async function oneGameToMongo(id){
  * @returns object of game detail infomation from Mongodb
  */
 async function findGameDetail(id){
-  await db.connect('620-reco-test1');
+  await db.connect('620-reco-test2');
   let game = await queries.findOneGame(id);
+  console.log(game);
   return game;
 }
-await oneGameToMongo(440);
+
+// await findGameDetail(10);
+// await oneGameToMongo(10);
+await steamGameToMongo();
+
 // export default {
 //   steamGameToMongo,
 //   oneGameToMongo,
