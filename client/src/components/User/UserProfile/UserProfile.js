87b5d428883fc1134a52777d63610018e43cae88
Paul Patrick Rabanal <paul7098@hotmail.com>
2023-03-01 09:01:10 -0500

    Modify styling

--- client/src/components/User/UserProfile/UserProfile.js
+++ client/src/components/User/UserProfile/UserProfile.js
@@ -1,13 +1,13 @@
 import Button from '../../UI/Button/Button';
 import styles from './UserProfile.module.css';
 
-function UserProfile({ username }) {
+function UserProfile() {
   return (
     <div className={styles['user-profile']}>
       <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" />
       <Button className={styles.settings}>User Settings</Button>
       <div className={styles['user-info']}>
-        <h2>{username}</h2>
+        <h2>USERNAME123</h2>
       </div>
     </div>
   );
