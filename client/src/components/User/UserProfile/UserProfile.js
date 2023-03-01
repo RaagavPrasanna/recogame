58100b1dd106a28f721c1eb76ded4e8a794bb3d5
Paul Patrick Rabanal <paul7098@hotmail.com>
2023-03-01 08:53:45 -0500

    Add flexbox for styling

--- client/src/components/User/UserProfile/UserProfile.js
+++ client/src/components/User/UserProfile/UserProfile.js
@@ -1,12 +1,13 @@
+import Button from '../../UI/Button/Button';
 import styles from './UserProfile.module.css';
 
 function UserProfile({ username }) {
   return (
     <div className={styles['user-profile']}>
       <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" />
+      <Button className={styles.settings}>User Settings</Button>
       <div className={styles['user-info']}>
         <h2>{username}</h2>
-        <span className={styles.settings}>User Settings</span>
       </div>
     </div>
   );
