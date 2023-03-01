76c8ef821ea61382ef6209000952ec312e15f1c3
Paul Patrick Rabanal <paul7098@hotmail.com>
2023-03-01 09:07:59 -0500

    Remove unnecessary import

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import Header from './components/Header/Header';
 import Home from './Home';
 import GameDetailView from './components/DetailViews/GameDetailView/GameDetailView';
 import UserProfile from './components/User/UserProfile/UserProfile';
-import UserSettings from './components/User/UserSettings/UserSettings';
 
 function App() {
   return (
