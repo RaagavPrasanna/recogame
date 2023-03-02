2d1ee2c26ded7548003e73c8b9c2f9758b810ecc
Paul Patrick Rabanal <paul7098@hotmail.com>
2023-03-02 13:25:51 -0500

    Add state hooks for account details

--- client/src/components/User/UserProfile/UserProfile.js
+++ client/src/components/User/UserProfile/UserProfile.js
@@ -1,10 +1,18 @@
 import Button from '../../UI/Button/Button';
 import styles from './UserProfile.module.css';
 import UserSettings from '../UserSettings/UserSettings';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function UserProfile() {
   const [isSettingsVisible, setIsSettingsVisible] = useState(false);
+  const [username, setUsername] = useState('');
+  const [accountType, setAccountType] = useState('');
+
+  useEffect(() => {
+    // TODO: Fetch account details from the server
+    setUsername('USERNAME123');
+    setAccountType('Steam Account');
+  });
 
   function showSettings() {
     setIsSettingsVisible(true);
@@ -17,12 +25,14 @@ function UserProfile() {
   return (
     <div className={styles['user-profile']}>
       <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" />
-      <Button onClick={showSettings} className={styles.settings}>User Settings</Button>
+      <Button onClick={showSettings} className={styles.settings}>
+        User Settings
+      </Button>
       <div className={styles['user-info']}>
-        <h2>USERNAME123</h2>
-        <h3>Account Type: Steam Account</h3>
+        <h2>{username}</h2>
+        <h3>Account Type: {accountType}</h3>
       </div>
-      {isSettingsVisible && <UserSettings onCancel={hideSettings}/>}
+      {isSettingsVisible && <UserSettings onCancel={hideSettings} />}
       <div className={styles['game-recommendations']}>
         <h2>Game Recommendations</h2>
         <hr></hr>
