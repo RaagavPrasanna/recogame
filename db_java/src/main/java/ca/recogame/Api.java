10ca3a10705c9a3e77bb1d6ebe0b5cf8f0689686
Shirley Maa <maashirley@gmail.com>
2023-03-02 13:28:15 -0500

    create menu for java with Api.java

---
+++ db_java/src/main/java/ca/recogame/Api.java
@@ -0,0 +1,98 @@
+package ca.recogame;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Api {
+
+  private static Scanner reader = new Scanner(System.in);
+  private Connection connect;
+  private NodeReader nodeReader;
+
+  public Api(String database) {
+    this.connect = new Connection(database);
+    this.nodeReader = new NodeReader();
+  }
+
+  public void run() {
+    // Used to check if menu should remain open
+    boolean keepMenu = true;
+
+    do {
+      System.out.println(
+          "1) Import all games \n" +
+          "2) Import one game details \n" +
+          "3) Import mutiple game details \n" +
+          "4) Exit\n");
+      // Manage response from user
+      int response = reader.nextInt();
+      keepMenu = manageResponse(response);
+    } while (keepMenu);
+
+    reader.close();
+  }
+
+  private boolean manageResponse(int response) {
+    // Closes menu if false
+    boolean keepMenu = true;
+
+    if (response == 1)
+      importListGames();
+    else if (response == 2)
+      importOneGameDetails();
+    else if (response == 3) {
+      importMulitGameDetails();  
+    }else
+      keepMenu = false;
+
+    return keepMenu;
+  }
+
+  private void importListGames() {
+    try {
+      List<Game> games = nodeReader.getAllGames("steam");
+      System.out.println(games.get(5).getAppid()); // --------------------remove
+      connect.insertManyGame(games);
+    } catch (Exception e) {
+      System.out.println("can not data shown : " + e);
+    }
+  }
+
+  private void importOneGameDetails() {
+    int id = reader.nextInt();
+    try {
+      GameDetails game = nodeReader.getOneGameDetails("steam", id);
+      System.out.println(game.getName()); // +------------------------remove
+      connect.insertOneGameDetails(game);
+    } catch (Exception e) {
+      System.out.println("can not data shown : " + e);
+    }
+  }
+
+  private void importMulitGameDetails() {
+    int[] idList = {
+        225840,
+        630,
+        224260,
+        13570,
+        440,
+        365450,
+        2280,
+        100,
+        367520,
+        506540,
+    };
+    List<GameDetails> games = new ArrayList<GameDetails>();
+    try {
+      for (int id : idList) {
+        GameDetails game = nodeReader.getOneGameDetails("steam", id);
+        games.add(game);
+      }
+      System.out.println(games.get(1).getName()); // --------------------remove
+      connect.insertManyGameDetails(games);
+    } catch (Exception e) {
+      System.out.println("can not data shown : " + e);
+    }
+  }
+}
--- db_java/src/main/java/ca/recogame/App.java
+++ db_java/src/main/java/ca/recogame/App.java
@@ -5,55 +5,17 @@ package ca.recogame;
  *
  */
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-
 
 public class App 
 {
     //this code is terrible! Always code in OO style with Java
-    public static void main( String[] args ) throws IOException, InterruptedException
-    {
-        // Game newGame = new Game(8891, "testJava4");
-        Connection connect = new Connection("620-reco-test2");
-        // connect.insertOneGame(newGame);
-    // // remove all data in all-games
-        // connect.deleteManyGame();
-     // // -------------------test for get all Games from node console-----------------------------------
-        // try{
-        //     NodeReader nReader = new NodeReader();
-        //     List<Game> games = nReader.getAllGames("steam");
-        //     System.out.println(games.get(5).getAppid());
-        //     // connect.insertOneGame(games.get(5));
-        //     connect.insertManyGame(games);
-        // }catch(Exception e){
-        //     System.out.println("can not data shown : "+e);
-        // }
-     // // ---------------test for get 1 game details----
-        // try{
-        //     NodeReader nReader = new NodeReader();
-        //     GameDetails game = nReader.getOneGameDetails("steam", 440);
-        //     System.out.println(game.getName());
-        //     connect.insertOneGameDetails(game);
-        // }catch(Exception e){
-        //    System.out.println("can not data shown : "+e);
-        // }      
-        // ----------------test of multi gameDetails-----------        
-        try{
-            NodeReader nReader = new NodeReader();
-            GameDetails game1 = nReader.getOneGameDetails("steam", 440);
-            GameDetails game2 = nReader.getOneGameDetails("steam", 10);
-            List<GameDetails> games = new ArrayList<GameDetails>();
-            games.add(game1);
-            
-            System.out.println(game1.getName());
-            connect.insertManyGameDetails(games);
-        }catch(Exception e){
-            System.out.println("can not data shown : "+e);
-        }      
-
+    public static void main( String[] args ) 
+        throws IOException, InterruptedException
+    {            
+       Api api = new Api("620-reco-test2");
+       api.run();
     }
 }
+
       
       
