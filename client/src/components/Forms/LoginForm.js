0128e1587d45bdd4c1e4526aafbd5bf80b9ad166
Raagav Prasanna <raagav.pras@gmail.com>
2023-03-04 17:57:27 -0500

    Implement steam user auth

--- client/src/components/Forms/LoginForm.js
+++ client/src/components/Forms/LoginForm.js
@@ -3,6 +3,7 @@ import Button from '../UI/Button/Button';
 import Modal from '../UI/Modal/Modal';
 import classes from './LoginForm.module.css';
 import { GoogleLogin } from '@react-oauth/google';
+// import { redirect } from "react-router-dom";
 
 function LoginForm({ onCancel }) {
   const [setUsername] = useState();
@@ -11,8 +12,19 @@ function LoginForm({ onCancel }) {
     e.preventDefault();
   };
 
+  const logout = async () => {
+    const res = await fetch('/authentication/logout');
+    if(res.status === 200) {
+      // eslint-disable-next-line no-alert
+      alert('Logged out');
+    } else {
+      // eslint-disable-next-line no-alert
+      alert('Failed to log out');
+    }
+  };
+
   const handleLogin = async googleData => {
-    const res = await fetch('/authentication/auth', {
+    const res = await fetch('/authentication/google-auth', {
       method: 'POST',
       body: JSON.stringify({
         token: googleData.credential
@@ -63,6 +75,7 @@ function LoginForm({ onCancel }) {
         <GoogleLogin onSuccess={handleLogin} onError={handleError}/>
         <br />
         <br />
+        <Button onClick={logout}>Logout</Button>
         <div>
           <Button onClick={onCancel}>Cancel</Button>
           <Button onSubmit={handleSubmit}>Sign In</Button>
--- server/src/express/routes/authentication.js
+++ server/src/express/routes/authentication.js
@@ -2,11 +2,36 @@ import express from 'express';
 import { OAuth2Client } from 'google-auth-library';
 import session from 'express-session';
 import { isAuthenticated, csrfProtect } from '../utils.js';
+import passport from 'passport';
+import passportSteam from 'passport-steam';
+
+const SteamStrategy = passportSteam.Strategy;
 const router = express.Router();
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 const users = new Array();
 
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.use(new SteamStrategy({
+  // Must be changed when deployed
+  returnURL: 'http://localhost:3001/authentication/steam-auth/return',
+  // Must be changed when deployed
+  realm: 'http://localhost:3001/',
+  apiKey: process.env.STEAM_API_KEY
+}, function (identifier, profile, done) {
+  process.nextTick(function () {
+    profile.identifier = identifier;
+    return done(null, profile);
+  });
+}
+));
 
 router.use(session({
   secret: process.env.SECRET,
@@ -21,7 +46,10 @@ router.use(session({
   }
 }));
 
-router.post('/auth', async (req, res) => {
+router.use(passport.initialize());
+router.use(passport.session());
+
+router.post('/google-auth', async (req, res) => {
   if(req.body === undefined) {
     return res.sendStatus(400);
   }
@@ -35,7 +63,8 @@ router.post('/auth', async (req, res) => {
   }
   const { name, email, picture } = ticket.getPayload();
 
-  const user = { name, email, picture };
+  const user = { name, email, picture, provider: 'google' };
+
 
   if(!users.some(u => u.email === user.email)) {
     users.push(user);
@@ -50,17 +79,36 @@ router.post('/auth', async (req, res) => {
   });
 });
 
-router.get('/protected', isAuthenticated, function(req, res) {
-  res.sendStatus(200);
+router.get('/steam-auth', passport.authenticate('steam', { failureRedirect: 'localhost:3000/' }), (req, res) => {
+  res.redirect('http://localhost:3000/');
+});
+
+router.get('/steam-auth/return', passport.authenticate('steam', { failureRedirect: 'localhost:3000/' }), (req, res) => {
+  console.log('in return');
+  req.session.regenerate((err) => {
+    if(err) {
+      return res.sendStatus(500);
+    }
+    req.session.user = req.user;
+    console.log('set session user');
+    console.log(req.session.user);
+    res.redirect('http://localhost:3000/');
+  });
+});
+
+router.get('/get-user', isAuthenticated, function(req, res) {
+  res.json(req.session.user);
 });
 
 router.get('/logout', isAuthenticated, function(req, res) {
   req.session.destroy(function(err) {
     if(err) {
+      console.error(err);
       return res.sendStatus(500);
     }
     res.clearCookie('id');
-    res.sendStatus(200);
+    console.log(req.user);
+    return res.sendStatus(200);
   });
 });
 
