b4a9dab9c2cc44e83eb616b781cb53cad635df64
Paul Patrick Rabanal <paul7098@hotmail.com>
2023-03-08 09:34:06 -0500

    Use feed endpoint to get game posts

--- client/src/store/PostsProvider.js
+++ client/src/store/PostsProvider.js
@@ -1,6 +1,15 @@
 import PostContext from './posts-context';
 import { useEffect, useState } from 'react';
 
+async function getGamePage(page, callback) {
+  const resp = await fetch(`/api/game/feed?page=${page}`);
+  if (!resp.ok) {
+    throw new Error(`Could not get page (${resp.status})`);
+  }
+  const data = await resp.json();
+  callback(data);
+}
+
 function PostsProvider({ children }) {
   const [posts, setPosts] = useState([]);
   // TODO: Add separate scoll position states for each page
@@ -8,46 +17,21 @@ function PostsProvider({ children }) {
   const [currPageHome, setCurrPageHome] = useState(0);
 
   useEffect(() => {
-    fetch('/api/game-all')
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error(`Error: (${res.status})`);
-        } else {
-          return res.json();
-        }
-      })
-      .then((data) =>
-        setPosts(
-          data.map((game, index) => {
-            return { ...game, id: index, rating: 5, price: 69.99 };
-          })
-        )
-      )
-      .catch((err) => console.error(err.message));
+    getGamePage(currPageHome, (data) => {
+      setPosts((prevPosts) => {
+        return [...prevPosts, ...data];
+      });
+      setCurrPageHome((currPage) => ++currPage);
+    });
   }, []);
 
   function fetchMoreData() {
-    setTimeout(() => {
+    getGamePage(currPageHome, (data) => {
       setPosts((prevPosts) => {
-        return prevPosts.concat(
-          Array.from({ length: 2 }).map(() => {
-            const id = Math.random();
-            return {
-              id,
-              name: 'Game Name',
-              devName: 'Dev Name',
-              imageHeader:
-                'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
-              shortDescription:
-                // eslint-disable-next-line max-len
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-              rating: 5,
-              price: 79.99,
-            };
-          })
-        );
+        return [...prevPosts, ...data];
       });
-    }, 1500);
+      setCurrPageHome((currPage) => ++currPage);
+    });
   }
 
   function handleScrollPosition() {
