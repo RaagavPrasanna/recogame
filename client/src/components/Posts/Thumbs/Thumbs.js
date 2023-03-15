b12adb9cc11182df01895c946cafab0baca090a8
Paul Patrick Rabanal <paul7098@hotmail.com>
2023-03-15 16:56:37 -0400

    Add callback for thumb click

--- client/src/components/Posts/Thumbs/Thumbs.js
+++ client/src/components/Posts/Thumbs/Thumbs.js
@@ -6,10 +6,24 @@ import {
 import styles from './Thumbs.module.css';
 
 function Thumbs() {
+  function thumbHandler(e) {
+    e.preventDefault();
+    const clicked = e.target;
+    const thumb =
+      clicked.closest('#thumbs-up') || clicked.closest('#thumbs-down');
+    if (!thumb) {
+      return;
+    }
+    console.log(thumb.id);
+  }
+
   return (
-    <div className={styles.thumbs}>
-      <BsFillHandThumbsUpFill className={styles['thumbs-up']} />
-      <BsFillHandThumbsDownFill className={styles['thumbs-down']} />
+    <div className={styles.thumbs} onClick={thumbHandler}>
+      <BsFillHandThumbsUpFill className={styles['thumbs-up']} id="thumbs-up" />
+      <BsFillHandThumbsDownFill
+        className={styles['thumbs-down']}
+        id="thumbs-down"
+      />
     </div>
   );
 }
