cc2e330042152349ca6e7079aa6da0fb4deac618
Paul Patrick Rabanal <paul7098@hotmail.com>
2023-03-17 08:53:13 -0400

    Add className prop to LanguageSelector

--- client/src/MultiLanguage/LanguageSelector.js
+++ client/src/MultiLanguage/LanguageSelector.js
@@ -4,16 +4,16 @@ import i18n from './I18n';
 const languages = {
   en: { nativeName: 'English' },
   fr: { nativeName: 'Français' },
-  zh: { nativeName: '中文简体' }
+  zh: { nativeName: '中文简体' },
 };
 
-const LanguageSelector = () => {
+const LanguageSelector = ({ className }) => {
   return (
-    <div>
-      <form type='submit' onSubmit={(e) => e.preventDefault()}>
+    <div className={className}>
+      <form type="submit" onSubmit={(e) => e.preventDefault()}>
         <select
           onChange={(e) => i18n.changeLanguage(e.target.value)}
-          style={{ backgroundColor:'#6E7F80' }}
+          style={{ backgroundColor: '#6E7F80' }}
         >
           {Object.keys(languages).map((lng) => (
             <option value={lng} key={lng}>
