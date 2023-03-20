323123d9c4766d8a49145c993fb0fd9a4a1f2244
Paul Patrick Rabanal <paul7098@hotmail.com>
2023-03-20 10:49:18 -0400

    Rename styling imports

--- client/src/components/Header/HamburgerToggle.js
+++ client/src/components/Header/HamburgerToggle.js
@@ -1,6 +1,6 @@
 import { motion } from 'framer-motion';
 import React from 'react';
-import styles from './MenuToggle.module.css';
+import styles from './HamburgerToggle.module.css';
 
 const Path = (props) => (
   <motion.path
