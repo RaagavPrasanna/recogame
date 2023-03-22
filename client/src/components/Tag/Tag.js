3ec08e775ca40e0c78791ecbd75d53be74609316
Paul Patrick Rabanal <paul7098@hotmail.com>
2023-03-22 09:49:17 -0400

    Add closing button

--- client/src/components/Tag/Tag.js
+++ client/src/components/Tag/Tag.js
@@ -1,7 +1,18 @@
 import styles from './Tag.module.css';
+import { useState } from 'react';
+import { BsXSquare } from 'react-icons/bs';
 
-function Tag({ tagName }) {
-  return <div className={styles.tag}>{tagName}</div>;
+function Tag({ tagName, closable }) {
+  return (
+    <div className={styles.tag}>
+      {`${tagName}`}
+      {closable && (
+        <span className={styles.close}>
+          <BsXSquare />
+        </span>
+      )}
+    </div>
+  );
 }
 
 export default Tag;
