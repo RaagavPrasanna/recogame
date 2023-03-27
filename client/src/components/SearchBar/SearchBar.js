48a83a135099e213160a98f8dabae014c3390b40
Liliane Tran Le <lylyanetl@hotmail.com>
2023-03-27 10:46:06 -0400

    Add redirect to fix issue with search bar

--- client/src/components/SearchBar/SearchBar.js
+++ client/src/components/SearchBar/SearchBar.js
@@ -3,12 +3,18 @@ import { Link } from 'react-router-dom';
 import styles from './SearchBar.module.css';
 import Modal from '../UI/Modal/Modal';
 
+
 function SearchBar(props) {
   const [userInput, setUserInput] = useState('');
   const [dataJson, setDataJson] = useState([]);
 
   function inputHandler(e) {
     setUserInput(e.target.value.toLowerCase());
+  }
+
+  function handleGame() {
+    props.handleShow;
+    location.redirect();
   }
 
   async function fetchGames() {
@@ -33,7 +39,7 @@ function SearchBar(props) {
       <input type="search" placeholder="Search Game" onChange={inputHandler} />
       {filteredData.slice(0, 10).map((game) => (
         <p key={game.id}>
-          <Link to={`/game/${game.id}`} onClick={props.handleShow}>
+          <Link to={`/game/${game.id}`} onClick={handleGame} >
             {game.name}
           </Link>
         </p>
